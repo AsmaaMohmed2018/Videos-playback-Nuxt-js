0a454f83cc61a15e805f32701d0535e2091b5869
Asmaa Mohamed <engineerasmaa01@gmail.com>
2020-04-18 15:53:43 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,77 @@
+<template>
+    <div id="player">Playback Videos</div> 
+</template>
+
+<script>
+export default {
+   created(){
+      var tag = document.createElement('script');
+      tag.src ="https://www.youtube.com/iframe_api";
+      var lastch= document.body.lastChild;
+      console.log(lastch);
+      lastch.parentNode.insertBefore(tag, lastch.nextSibling);
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          height: '390',
+          width: '640',
+          videoId: 'M7lc1UVf-VE',
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        });
+      }
+      function onPlayerReady(event) {
+        event.target.playVideo();
+      }
+      var done = false;
+      function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.PLAYING && !done) {
+          setTimeout(stopVideo, 6000);
+          done = true;
+        }
+      }
+      function stopVideo() {
+        player.stopVideo();
+      }
+      
+   }
+
+
+    
+
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: left ;
+  align-items: left;
+  text-align: left;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
